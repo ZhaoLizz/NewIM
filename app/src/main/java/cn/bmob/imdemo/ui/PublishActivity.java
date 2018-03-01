770c3030259b2efa2ce15e762b166e598d46e154
a6100890 <499531245@qq.com>
2018-03-01 09:39:16 +0800

    1

--- app/src/main/java/cn/bmob/imdemo/ui/PublishActivity.java
+++ app/src/main/java/cn/bmob/imdemo/ui/PublishActivity.java
@@ -1,25 +1,24 @@
 package cn.bmob.imdemo.ui;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
+
 import com.orhanobut.logger.Logger;
 
 import butterknife.Bind;
 import cn.bmob.imdemo.R;
 import cn.bmob.imdemo.base.BaseActivity;
 import cn.bmob.imdemo.ui.fragment.HomeFragment;
-import cn.bmob.imdemo.util.ViewUtil;
+import cn.bmob.imdemo.util.HttpUtils;
 
 
-public class PublishActivity extends BaseActivity {
+public class PublishActivity extends BaseActivity implements HttpUtils.RecognizeListener {
     private Bitmap mPhotoBitmap;
+    private static final String TAG = "PublishActivity";
 
     @Bind(R.id.publish_good_img)
     ImageView img_publish_good;
@@ -40,6 +39,7 @@ public class PublishActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish);
         initView();
+
     }
 
 
@@ -48,6 +48,30 @@ public class PublishActivity extends BaseActivity {
         mPhotoBitmap = getIntent().getParcelableExtra(HomeFragment.EXTRA_BITMAP);
         if (mPhotoBitmap != null) {
             img_publish_good.setImageBitmap(mPhotoBitmap);
+            recognizeImage(this, mPhotoBitmap);
         }
+    }
+
+    private void recognizeImage(HttpUtils.RecognizeListener listener, Bitmap bitmap) {
+        listener.onRecognize(bitmap);
+    }
+
+
+    @Override
+    public String onRecognize(final Bitmap bitmap) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String jsonBody = HttpUtils.Bitmap2Json(bitmap, 100);
+                String jsonResult = "";
+                try {
+                    jsonResult = HttpUtils.sendPost(jsonBody);
+                    Logger.json(jsonResult);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        return "";
     }
 }
